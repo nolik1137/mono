f466a973ec2e56aab137620d3251c377fc7dedf2
nolik1137 <127095908+nolik1137@users.noreply.github.com>
2023-06-23 10:29:06 +0300

    Add test.php

---
+++ vel/java/java.js
@@ -0,0 +1,26 @@
+document.getElementById("registerLink").addEventListener("click", function(e) {
+  e.preventDefault();
+  var registrationFormOverlay = document.getElementById("registrationFormOverlay");
+  registrationFormOverlay.style.display = "block";
+});
+
+document.addEventListener("click", function(e) {
+  var registrationFormOverlay = document.getElementById("registrationFormOverlay");
+  if (e.target === registrationFormOverlay) {
+    registrationFormOverlay.style.display = "none";
+  }
+});
+
+
+
+document.getElementById("loginLink").addEventListener("click", function(event) {
+  event.preventDefault();
+  document.getElementById("loginFormOverlay").style.display = "block";
+});
+
+document.getElementById("loginFormOverlay").addEventListener("click", function(event) {
+  if (event.target === this) {
+    this.style.display = "none";
+  }
+});
+
